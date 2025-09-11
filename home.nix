0f7680093751cf8ac8b50cfc33285259b75be935
Viktor Palmkvist <vipa@kth.se>
2025-09-11 14:15:30 +0200

    Abbreviation for "last command" (via !!) in fish

--- home.nix
+++ home.nix
@@ -725,6 +725,11 @@ rec {
         echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
       end
       abbr --add dotdot --regex '^\.\.+$' --function multicd
+
+      function last_history_item
+          echo $history[1]
+      end
+      abbr -a !! --position anywhere --function last_history_item
     '';
     plugins = with pkgs.fishPlugins; [
       { name = "tide"; src = tide.src; }
